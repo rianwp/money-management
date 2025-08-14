a66d6d549e013b95f076aa980a711a9ad529e62d
rianwpzx <rianwp2015@gmail.com>
2025-08-15 01:11:14 +0700

    fix: refactor name use-mobile to useMobile

--- src/components/Dashboard/Header/index.tsx
+++ src/components/Dashboard/Header/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useIsMobile } from '@/hooks/use-mobile'
+import { useIsMobile } from '@/hooks/useMobile'
 import { Separator } from '../../ui/separator'
 import { SidebarTrigger } from '../../ui/sidebar'
 
